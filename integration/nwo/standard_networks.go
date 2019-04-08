aa55f9746e719f8ca6b371a75dff611eb3d15b9a
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-04-08 15:02:59 +0530

    basic elastico func added

--- integration/nwo/standard_networks.go
+++ integration/nwo/standard_networks.go
@@ -89,6 +89,89 @@ func BasicSolo() *Config {
 	}
 }
 
+func BasicElastico() *Config {
+	return &Config{
+		Organizations: []*Organization{{
+			Name:          "OrdererOrg",
+			MSPID:         "OrdererMSP",
+			Domain:        "example.com",
+			EnableNodeOUs: false,
+			Users:         0,
+			CA:            &CA{Hostname: "ca"},
+		}, {
+			Name:          "Org1",
+			MSPID:         "Org1MSP",
+			Domain:        "org1.example.com",
+			EnableNodeOUs: true,
+			Users:         2,
+			CA:            &CA{Hostname: "ca"},
+		}, {
+			Name:          "Org2",
+			MSPID:         "Org2MSP",
+			Domain:        "org2.example.com",
+			EnableNodeOUs: true,
+			Users:         2,
+			CA:            &CA{Hostname: "ca"},
+		}},
+		Consortiums: []*Consortium{{
+			Name: "SampleConsortium",
+			Organizations: []string{
+				"Org1",
+				"Org2",
+			},
+		}},
+		Consensus: &Consensus{
+			Type: "elastico",
+		},
+		OrdererCap: &OrdererCapabilities{
+			V2_0: false,
+		},
+		SystemChannel: &SystemChannel{
+			Name:    "systemchannel",
+			Profile: "TwoOrgsOrdererGenesis",
+		},
+		Orderers: []*Orderer{
+			{Name: "orderer", Organization: "OrdererOrg"},
+		},
+		Channels: []*Channel{
+			{Name: "testchannel", Profile: "TwoOrgsChannel"},
+		},
+		Peers: []*Peer{{
+			Name:         "peer0",
+			Organization: "Org1",
+			Channels: []*PeerChannel{
+				{Name: "testchannel", Anchor: true},
+			},
+		}, {
+			Name:         "peer1",
+			Organization: "Org1",
+			Channels: []*PeerChannel{
+				{Name: "testchannel", Anchor: false},
+			},
+		}, {
+			Name:         "peer0",
+			Organization: "Org2",
+			Channels: []*PeerChannel{
+				{Name: "testchannel", Anchor: true},
+			},
+		}, {
+			Name:         "peer1",
+			Organization: "Org2",
+			Channels: []*PeerChannel{
+				{Name: "testchannel", Anchor: false},
+			},
+		}},
+		Profiles: []*Profile{{
+			Name:     "TwoOrgsOrdererGenesis",
+			Orderers: []string{"orderer"},
+		}, {
+			Name:          "TwoOrgsChannel",
+			Consortium:    "SampleConsortium",
+			Organizations: []string{"Org1", "Org2"},
+		}},
+	}
+}
+
 func BasicKafka() *Config {
 	config := BasicSolo()
 	config.Consensus.Type = "kafka"
