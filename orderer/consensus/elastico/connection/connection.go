23e6db762a23d11854cc94b43ea3ce9ed5ef215e
ayushiagg <ayushiagg@cse.iitk.ac.in>
2019-04-12 12:33:43 +0530

    all queues file added to give all queues of rmq connection.go added to make rmq connection and channel

---
+++ orderer/consensus/elastico/connection/connection.go
@@ -0,0 +1,38 @@
+package connection
+
+import (
+	"os"
+
+	"github.com/hyperledger/fabric/common/flogging"
+	"github.com/streadway/amqp"
+)
+
+var logger = flogging.MustGetLogger("orderer.consensus.elastico.connection")
+
+func FailOnError(err error, msg string, exit bool) {
+	// logging the error
+	if err != nil {
+		logger.Error(msg)
+		if exit {
+			os.Exit(1)
+		}
+	}
+}
+
+func GetChannel(connection *amqp.Connection) *amqp.Channel {
+	/*
+		get channel
+	*/
+	channel, err := connection.Channel()               // create a channel
+	FailOnError(err, "Failed to open a channel", true) // report the error
+	return channel
+}
+
+func GetConnection() *amqp.Connection {
+	/*
+		establish a connection with RabbitMQ server
+	*/
+	connection, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	FailOnError(err, "Failed to connect to RabbitMQ", true) // report the error
+	return connection
+}
